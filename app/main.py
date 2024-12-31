5e4ad74a252ba86b1d5dd4d35b60ced046ee83ad
Michael <michaelkenjetsyan@gmail.com>
2024-12-31 18:49:57 +0400

    added ui file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from .db import Base, engine
+from sqlalchemy.orm import Session  # Добавьте импорт Session
+from fastapi import FastAPI, Depends
+from .db import Base, engine, get_db
 from .models import Car, Mechanic, Order
 from .api import router
 
@@ -11,5 +12,23 @@ Base.metadata.create_all(bind=engine)
 @app.get("/")
 def read_root():
     return {"message": "API is running!"}
+
+@app.get("/orders/")
+def get_orders(db: Session = Depends(get_db)):
+    orders = db.query(Order).all()
+    return orders
+
+@app.get("/cars/")
+def get_cars(db: Session = Depends(get_db)):
+    cars = db.query(Car).all()
+    return cars
+
+
+# Эндпоинт для получения всех механиков
+@app.get("/mechanics/")
+def get_mechanics(db: Session = Depends(get_db)):
+    mechanics = db.query(Mechanic).all()
+    return mechanics
+
 
 app.include_router(router)
